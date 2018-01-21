a4bbb52134ddbb56c91fe04d91e2404effc0349c
Jonathan <jonathanlaniado@me.com>
2018-01-21 03:06:50 -0500

    Updated 3872x2592-subway.jpgindex.html script.js styles.css .

---
+++ script.js
@@ -0,0 +1,26 @@
+var bgImages = [];
+
+// adds all image locations from the assets/backgrounds/ directory to an array
+$.ajax({
+  url: "assets/backgrounds/",
+  async: false,
+  success: function(data) {
+    $(data).find("a:contains(.jpg)").each(function() {
+      bgImages.push($(this).attr("href"));
+    });
+  }
+});
+
+// calculates a random number between the specified values
+function getRand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$(document).ready(function() {
+  var rand = getRand(0, bgImages.length);
+  $('html').css('background-image', 'url(' + bgImages[rand] + ')');
+  $('html').css('background-position', 'center');
+  $('html').css('background-size', 'center');
+  $('html').css('background-repeat', 'no-repeat');
+  $('html').css('background-attachment', 'fixed');
+});
