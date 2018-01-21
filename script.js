665859ae30fc4f0171d23a0558c0cc3faf25fa9c
Jonathan <jonathanlaniado@me.com>
2018-01-21 13:24:06 -0500

    Updated script.js.

--- script.js
+++ script.js
@@ -33,14 +33,14 @@ jQuery.ajax({
 // animates several elements on the page
 $(document).ready(function() {
   $('.hamburger').click(function() {
-    // $('.hamburger').toggleClass('is-active');
-// if $('.hb-menu').hasClass('menu-selected') {
-//   $('.hb-menu').toggleClass('slide-out-top');
-// }
-// else {
-//   $('.hb-menu').toggleClass('menu-selected');
-//   $('.hb-menu').toggleClass('slide-in-top');
-// }
+    $('.hamburger').toggleClass('is-active');
+if $('.hb-menu').hasClass('menu-selected') {
+  $('.hb-menu').toggleClass('slide-out-top');
+}
+else {
+  $('.hb-menu').toggleClass('menu-selected');
+  $('.hb-menu').toggleClass('slide-in-top');
+}
   });
   $('.logo').click(function() {
     $('.logo').addClass('rotate-center');
