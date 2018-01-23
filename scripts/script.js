2ff291a5e07438076ca779719c55a03a9abf396c
Jonathan <jonathanlaniado@me.com>
2018-01-23 15:03:12 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,37 @@
 // animates the navigation bar on hover
 $(document).ready(function() {
 
+});
+
+// animates the hamburger menu popup
+$(document).ready(function() {
+  $('.hamburger').click(function() {
+    $('.hamburger').toggleClass('is-active');
+    $('.hamburger-inner').toggleClass('white');
+    if ($('.hb-menu').hasClass('menu-selected')) {
+      $('.hb-menu').toggleClass('slide-out-top');
+    } else {
+      $('.hb-menu').toggleClass('menu-selected');
+      $('.hb-menu').toggleClass('slide-in-top');
     }
+  });
+});
 
-    // animates the hamburger menu popup
-    $(document).ready(function() {
-      $('.hamburger').click(function() {
-        $('.hamburger').toggleClass('is-active');
-        $('.hamburger-inner').toggleClass('white');
-        if ($('.hb-menu').hasClass('menu-selected')) {
-          $('.hb-menu').toggleClass('slide-out-top');
-        } else {
-          $('.hb-menu').toggleClass('menu-selected');
-          $('.hb-menu').toggleClass('slide-in-top');
-        }
-      });
-    });
+// closes the hamburger menu if the window is resized
+$(window).resize(function() {
+  if (($(window).width() > 813) && ($('.hb-menu').hasClass('menu-selected'))) {
+    $('.hb-menu').addClass('slide-out-top');
+    $('.hamburger').removeClass('is-active');
+    $('.hamburger-inner').removeClass('white');
+  }
+});
 
-    // closes the hamburger menu if the window is resized
-    $(window).resize(function() {
-      if (($(window).width() > 813) && ($('.hb-menu').hasClass('menu-selected'))) {
-        $('.hb-menu').addClass('slide-out-top');
-        $('.hamburger').removeClass('is-active');
-        $('.hamburger-inner').removeClass('white');
-      }
-    });
-
-    // animates the logo on the homepage
-    $(document).ready(function() {
-      $('.logo').click(function() {
-        $('.logo').addClass('rotate-center');
-        setTimeout(function() {
-          $('.logo').removeClass('rotate-center');
-        }, 400);
-      });
-    });
+// animates the logo on the homepage
+$(document).ready(function() {
+  $('.logo').click(function() {
+    $('.logo').addClass('rotate-center');
+    setTimeout(function() {
+      $('.logo').removeClass('rotate-center');
+    }, 400);
+  });
+});
