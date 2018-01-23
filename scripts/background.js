63a000a5bd09bada7d22dc99aec3d780fab58ec1
Jonathan <jonathanlaniado@me.com>
2018-01-23 03:01:03 -0500

    Updated background.js.

---
+++ scripts/background.js
@@ -0,0 +1,30 @@
+// calculates a random number between the specified values
+function getRand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// sets a random picture from assets/backgrounds as the homepage background
+jQuery.ajax({
+    url: "https://api.github.com/repos/jonathanlaniado/site/contents/assets/backgrounds",
+    type: "GET",
+    headers: {
+      "Authorization": "Basic am9uYXRoYW5sYW5pYWRvOlpaN21KOTJvZGxEZjBOQw==",
+    },
+  })
+  .done(function(data, textStatus, jqXHR) {
+    var paths = [];
+    Object.keys(data).forEach(function(key) {
+      var val = data[key]["path"];
+      paths.push(val);
+    });
+    paths.shift();
+    var rand = getRand(0, paths.length);
+    $('html').css('background-image', 'url(' + paths[rand] + ')');
+    $('html').css('background-position', 'center');
+    $('html').css('background-size', 'center');
+    $('html').css('background-repeat', 'no-repeat');
+    $('html').css('background-attachment', 'fixed');
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log("HTTP Request Failed");
+  });
