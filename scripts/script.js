0c7bd04236dd293d7ff2f15ff69368203a505abf
Jonathan <jonathanlaniado@me.com>
2018-02-27 13:56:30 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $('.hamburger').click(function() {
     $('.hamburger').toggleClass('is-active');
     $('.hamburger-inner').toggleClass('white');
-    $('body').css('overflow', 'auto');
+    $('body').css('overflow', 'visible');
     if ($('.hb-menu').hasClass('menu-selected')) {
       $('.hb-menu').toggleClass('slide-out-top');
     } else {
