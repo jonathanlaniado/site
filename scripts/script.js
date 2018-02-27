e69f7e09b1d5943cff1be4b9aa07f8e590e1a234
Jonathan <jonathanlaniado@me.com>
2018-02-27 15:08:17 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,46 @@
 var hbMenuActive = false;
 // animates the hamburger menu popup
 $(document).ready(function() {
-      $('.hamburger').click(function() {
-          hbMenuActive = !hbMenuActive;
-          $('.hamburger').toggleClass('is-active');
-          $('.hamburger-inner').toggleClass('white');
-          $('body').toggleClass('no-scroll');
-          if ($('.hb-menu').hasClass('menu-revealed')) {
-            $('.hb-menu').toggleClass('slide-out-top');
-          } else {
-            $('.hb-menu').toggleClass('menu-revealed');
-            $('.hb-menu').toggleClass('slide-in-top');
-          }
-          if ($('.hb-menu').hasClass('slide-out-top')) {
-            $('html, body').off('touchmove', function(e) {
-              e.preventDefault();
-            });
-            console.log("No!");
-          } else {
-            $('html, body').on('touchmove', function(e) {
-              e.preventDefault();
-            });
-            console.log("Yes!")
-          });
-      });
-
-    // closes the hamburger menu if the window is resized
-    $(window).resize(function() {
-      if (($(window).width() > 813) && ($('.hb-menu').hasClass('menu-revealed'))) {
-        $('.hb-menu').addClass('slide-out-top');
-        $('.hamburger').removeClass('is-active');
-        $('.hamburger-inner').removeClass('white');
+  $('.hamburger').click(function() {
+      hbMenuActive = !hbMenuActive;
+      $('.hamburger').toggleClass('is-active');
+      $('.hamburger-inner').toggleClass('white');
+      $('body').toggleClass('no-scroll');
+      if ($('.hb-menu').hasClass('menu-revealed')) {
+        $('.hb-menu').toggleClass('slide-out-top');
+      } else {
+        $('.hb-menu').toggleClass('menu-revealed');
+        $('.hb-menu').toggleClass('slide-in-top');
       }
-    });
-
-    // animates the logo on the homepage
-    $(document).ready(function() {
-      $('.logo').click(function() {
-        $('.logo').addClass('rotate-center');
-        setTimeout(function() {
-          $('.logo').removeClass('rotate-center');
-        }, 400);
+      if ($('.hb-menu').hasClass('slide-out-top')) {
+        $('html, body').off('touchmove', function(e) {
+          e.preventDefault();
+        });
+        console.log("No!");
+      } else {
+        $('html, body').on('touchmove', function(e) {
+          e.preventDefault();
+        });
+        console.log("Yes!")
       });
-    });
+  });
+});
+
+// closes the hamburger menu if the window is resized
+$(window).resize(function() {
+  if (($(window).width() > 813) && ($('.hb-menu').hasClass('menu-revealed'))) {
+    $('.hb-menu').addClass('slide-out-top');
+    $('.hamburger').removeClass('is-active');
+    $('.hamburger-inner').removeClass('white');
+  }
+});
+
+// animates the logo on the homepage
+$(document).ready(function() {
+  $('.logo').click(function() {
+    $('.logo').addClass('rotate-center');
+    setTimeout(function() {
+      $('.logo').removeClass('rotate-center');
+    }, 400);
+  });
+});
