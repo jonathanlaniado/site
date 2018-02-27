e9db19c312e1b7ef4cd407bf0c4040515c46fd37
Jonathan <jonathanlaniado@me.com>
2018-02-27 14:27:16 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,20 @@ $('html, body').on('touchmove', function(e) {
   e.preventDefault();
 });
 
-var hbMenuActive;
+var hbMenuActive = false;
 
 // animates the hamburger menu popup
 $(document).ready(function() {
   $('.hamburger').click(function() {
+    hbMenuActive.toggle(true);
     $('.hamburger').toggleClass('is-active');
     $('.hamburger-inner').toggleClass('white');
     $('body').toggleClass('no-scroll');
     if ($('.hb-menu').hasClass('menu-selected')) {
       $('.hb-menu').toggleClass('slide-out-top');
-      hbMenuActive = false;
     } else {
       $('.hb-menu').toggleClass('menu-selected');
       $('.hb-menu').toggleClass('slide-in-top');
-      hbMenuActive = true;
     }
   });
 });
