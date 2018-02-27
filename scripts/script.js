746f8a75b6f572f8cfcb1fd7b53b2934a03a9abf
Jonathan <jonathanlaniado@me.com>
2018-02-27 14:58:27 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
     } else {
       $('.hb-menu').toggleClass('menu-revealed');
       $('.hb-menu').addClass('slide-in-top');
-      $('.hb-menu').removeClass('slide-in-top');
       $('.hb-menu').removeClass('slide-out-top');
       $('html, body').on('touchmove', function(e) {
         e.preventDefault();
