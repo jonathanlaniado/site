87604cf38811877ab236dadc77f68e3e52f2b085
Jonathan <jonathanlaniado@me.com>
2018-02-27 16:06:18 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,17 @@ $(document).ready(function() {
   $('.hamburger').click(function() {
     $('.hamburger').toggleClass('is-active');
     $('.hamburger-inner').toggleClass('white');
+    $('body').toggleClass('no-scroll');
     if ($('.hb-menu').hasClass('menu-revealed')) {
       $('.hb-menu').toggleClass('slide-out-top');
     } else {
       $('.hb-menu').toggleClass('menu-revealed');
       $('.hb-menu').toggleClass('slide-in-top');
+    }
+    if ($('.hb-menu').hasClass('slide-out-top')) {
+      $('html, body').off('touchmove', false);
+    } else {
+      $('html, body').on('touchmove', false);
     }
   });
 });
