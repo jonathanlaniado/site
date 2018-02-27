cd1ebccb5c0481509d407ceeabf8f5a745a59c0c
Jonathan <jonathanlaniado@me.com>
2018-02-27 13:48:05 -0500

    Updated script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     $('.hamburger').toggleClass('is-active');
     $('.hamburger-inner').toggleClass('white');
     $('.hamburger').off('scroll mousewheel touchmove', stopScrolling);
+    $('body').off('scroll mousewheel touchmove', stopScrolling);
     if ($('.hb-menu').hasClass('menu-selected')) {
       $('.hb-menu').toggleClass('slide-out-top');
     } else {
